75b004e199102027b5fa12ff81d8032fb545c0f8
amanbhattsquare <aman.bhattsquare@gmail.com>
2025-11-12 15:16:27 +0530

    Add face15.jpg image to static/images directory

--- GYM/urls.py
+++ GYM/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.user_login, name='login'),
     path('dashboard', views.dashboard, name="dashboard")
 ]
 
--- GYM/views.py
+++ GYM/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-def dashboard (request):
-      return render(request, "dashboard.html")
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'registration/login.html')
